948c5dd199e146c10964259131d27175b74f5d97
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2025-01-08 12:27:03 +0530

    initial commit

--- Demo.ts
+++ Demo.ts
@@ -1,25 +1,43 @@
 import http from 'http';
+import fs from 'fs';
 // import fs from 'fs';
 
 // const server = http.createServer((req,res) => {
 //     console.log(req.url,req.method);
 //     fs.writeFileSync('demo.text',( req.url+':' + req.method));
 //     process.exit();
+// });import http from 'http';
+
+
+// const server = http.createServer((req,res)=>{
+//     console.log(req.url, req.method);
+
+//     if(req.url === "/add"){
+//         res.setHeader('content-type','text.html')
+//         res.write('<html><head><h1>Add Customer</h1></head></html>')
+//         return res.end
+//     }else{
+//         res.setHeader('content-type','text.html')
+//         res.write('<html><head><h1>Dashboard</h1></head></html>')
+//         res.end
+//     }
+//     if(req.url === "/item"&& req.method === "POST"){
+//         res.write('Items List')
+//         res.end();
+//     }
 // });
-const server = http.createServer((req,res) => {
-    
-    if(req.url === '/add'){
-        console.log(req.url,req.method);
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html><head><h1>Add Customer Form</h1></head></html>');
+const server = http.createServer((req,res)=>{
+    console.log(req.url, req.method);
+
+    if(req.url === "/add"){
+                res.setHeader('content-type','text.html')
+                res.write('<html><body><form action="/items" method ="POST"><input type="text"name="item"/><button type="submit">Submit</butto ></form></body></html>')
+                res.end
+            }
+    if(req.url === "/item"&& req.method === "POST"){
+        res.write('Items List')
         res.end();
-        process.exit();
-    }else{
-        console.log(req.url,req.method);
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html><head><h1>Add Customer Form</h1></head></html>');
-        res.end();
-        process.exit();
     }
 });
+
 server.listen(3000);
